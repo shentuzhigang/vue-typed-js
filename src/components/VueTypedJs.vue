83efcf4e4fb19fbf634a7579f5003c66b7ff6a2d
shentuzhigang <shentuzhigang01@weidian.com>
2022-09-12 13:23:05 +0800

    fix: fix when reload

--- src/components/VueTypedJs.vue
+++ src/components/VueTypedJs.vue
@@ -1,48 +1,190 @@
-<template>
-  <div class="typed-element" ref="typedElement">
-    <slot></slot>
-  </div>
-</template>
-
 <script>
 import Typed from 'typed.js'
-import { props, getEventHandlers } from '../config/typed-component.config'
 
 export default {
-  name: 'vue-typed-js',
-  props,
-  data () {
+  name: 'VueTypedJs',
+  props: {
+    strings: {
+      type: Array,
+      required: false,
+      default: function () {
+        return ['Hello World!']
+      },
+    },
+    stringsElement: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    typeSpeed: {
+      type: Number,
+      required: false,
+      default: 50,
+    },
+    startDelay: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    backSpeed: {
+      type: Number,
+      required: false,
+      default: 30,
+    },
+    smartBackspace: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    shuffle: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    backDelay: {
+      type: Number,
+      required: false,
+      default: 700,
+    },
+    fadeOut: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    fadeOutClass: {
+      type: String,
+      required: false,
+      default: 'typed-fade-out',
+    },
+    fadeOutDelay: {
+      type: Number,
+      required: false,
+      default: 500,
+    },
+    loop: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    loopCount: {
+      type: Number,
+      required: false,
+      default: Infinity,
+    },
+    showCursor: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    cursorChar: {
+      type: String,
+      required: false,
+      default: '|',
+    },
+    autoInsertCss: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    attr: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    bindInputFocusEvents: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    contentType: {
+      type: String,
+      required: false,
+      default: 'html',
+    },
+  },
+  emits: [
+    'onStart',
+    'onComplete',
+    'preStringTyped',
+    'onStringTyped',
+    'onLastStringBackspaced',
+    'onTypingPaused',
+    'onTypingResumed',
+    'onReset',
+    'onStop',
+    'onDestroy',
+  ],
+  data() {
     return {
       typedObj: null,
     }
   },
+  mounted: function () {
+    this.initTypedJS()
+  },
+  updated() {
+    if (this.typedObj !== undefined) {
+      this.typedObj.destroy()
+      this.initTypedJS()
+    }
+  },
   methods: {
+    getEventHandlers() {
+      return {
+        onComplete: () => {
+          this.$emit('onComplete')
+        },
+        preStringTyped: () => {
+          this.$emit('preStringTyped')
+        },
+        onStringTyped: () => {
+          this.$emit('onStringTyped')
+        },
+        onLastStringBackspaced: () => {
+          this.$emit('onLastStringBackspaced')
+        },
+        onTypingPaused: () => {
+          this.$emit('onTypingPaused')
+        },
+        onTypingResumed: () => {
+          this.$emit('onTypingResumed')
+        },
+        onReset: () => {
+          this.$emit('onReset')
+        },
+        onStop: () => {
+          this.$emit('onStop')
+        },
+        onStart: () => {
+          this.$emit('onStart')
+        },
+        onDestroy: () => {
+          this.$emit('onDestroy')
+        },
+      }
+    },
     throwError: function (message) {
       throw new TypeError(message)
     },
     initTypedJS: function () {
-      const $typed = this.$refs.typedElement.querySelector('.typing')
-
-      if (this.$slots.default.length > 1) {
-        this.throwError(`Just one child element allowed inside <${this.$options.name}> component.`)
-      } else if (this.$slots.default.length === 1) {
-        let typedConfig = this.$props
-        typedConfig = getEventHandlers(this, typedConfig)
-        this.typedObj = new Typed($typed, typedConfig)
-      }
+      const typedConfig = this.$props
+      this.typedObj = new Typed(this.$refs.typedElement, {
+        ...this.getEventHandlers(),
+        ...typedConfig,
+      })
     },
-  },
-  mounted () {
-    this.initTypedJS()
-  },
-  destroyed () {
-    this.typedObj.destroy()
   },
 }
 </script>
 
+<template>
+  <div className="typed">
+    <span ref="typedElement"></span>
+  </div>
+</template>
+
 <style lang="scss">
-.typed-element {
+.typed {
   display: flex;
   align-items: center;
 
@@ -52,7 +194,9 @@ export default {
   }
 }
 
-@keyframes typedjsBlink{
-  50% { opacity: 0.0; }
+@keyframes typedjsBlink {
+  50% {
+    opacity: 0;
+  }
 }
 </style>
